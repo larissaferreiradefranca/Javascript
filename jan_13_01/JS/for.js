74260f6579810806eb3ba47c98fae294ee1a6487
Larissa Ferreira de França <larissaferreiradefranca@gmail.com>
2026-01-22 18:30:20 -0300

    Initial Projects

--- jan_13_01/JS/for.js
+++
@@ -1,5 +0,0 @@
-// Estrutura: for (início; condição; incremento)
-for (let i = 0; i <= 10; i++) {
-    console.log(i)
-}
-// Resultado: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
--- jan_13_01/JS/while.js
+++
@@ -1,8 +0,0 @@
-//WHILE: Enquanto a condiçãoo for verdadeira
-
-let contador = 0;
-
-while (contador < 3) {
-    console.log(contador);
-    contador++;
-}
